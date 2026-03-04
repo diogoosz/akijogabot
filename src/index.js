473d2a6d7ae34e150aef2ba51fa9373447366715
diogoosz <diogomarciano40@gmail.com>
2026-03-04 20:25:39 -0300

    versao final do bot

---
+++ src/index.js
@@ -0,0 +1,88 @@
+import makeWASocket, {
+  DisconnectReason,
+  fetchLatestWaWebVersion,
+  useMultiFileAuthState,
+} from "@whiskeysockets/baileys";
+import P from "pino";
+import readline from "readline";
+import { mensagensEnviadas } from "./respostas.js";
+
+const question = (message) => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  return new Promise((resolve) =>
+    rl.question(message, (answer) => {
+      rl.close();
+      resolve(answer);
+    }),
+  );
+};
+
+const onlyNumbers = (text) => text.replace(/[^0-9]/g, "");
+
+const { version, isLatest } = await fetchLatestWaWebVersion();
+
+async function startBot() {
+  const { state, saveCreds } = await useMultiFileAuthState("auth_info");
+
+  const sock = makeWASocket({
+    version,
+    auth: state,
+    logger: P({ level: "error" }),
+    browser: ["Ubuntu", "Chrome", "1.0.0"],
+    syncFullHistory: false,
+    markOnlineOnConnect: true,
+    shouldIgnoreJid: (jid) => {
+      return jid?.endsWith("@g.us") || jid === "status@broadcast";
+    },
+  });
+
+  if (!sock.authState.creds.registered) {
+    const phoneNumber = await question(
+      "Digite o número do WhatsApp (ex: 5511999999999): ",
+    );
+
+    if (!phoneNumber) {
+      throw new Error("Número de telefone inválido");
+    }
+    const code = await sock.requestPairingCode(onlyNumbers(phoneNumber));
+    console.log(`\nSeu código de conexão é: ${code}\n`);
+  }
+  sock.ev.on("connection.update", async (update) => {
+    const { connection, lastDisconnect } = update;
+
+    if (connection === "close") {
+      const shouldReconnect =
+        lastDisconnect?.error?.output?.statusCode !==
+        DisconnectReason.loggedOut;
+      if (shouldReconnect) startBot();
+    } else if (connection === "open") {
+      console.log("O bot akijoga está pronto para uso");
+    }
+  });
+  sock.ev.on("creds.update", saveCreds);
+
+  sock.ev.on("messages.upsert", async ({ messages, type }) => {
+    if (type !== "notify") return;
+
+    for (const m of messages) {
+      if (!m.message) continue;
+      const jid = m.key.remoteJid;
+      if (jid === "status@broadcast" || jid.endsWith("@g.us")) continue;
+
+      const fromMe = m.key.fromMe;
+      const nome = m.pushName || "Cliente";
+
+      await new Promise((resolve) => setTimeout(resolve, 1500));
+
+      console.log("Enviada pelo admin: ", fromMe);
+      console.log("Jid: ", jid);
+
+      await mensagensEnviadas(sock, m);
+    }
+  });
+}
+
+startBot();
---
+++ src/respostas.js
@@ -0,0 +1,74 @@
+const atendimentos = new Map();
+
+export async function mensagensEnviadas(sock, m) {
+  const textoOriginal =
+    m.message?.conversation || m.message?.extendedTextMessage?.text || "";
+  const msg = textoOriginal.toLowerCase().trim();
+
+  const jid = m.key.remoteJid;
+  const fromMe = m.key.fromMe;
+  const nome = m.pushName || "Usuário";
+
+  if (fromMe && msg === "obrigado!") {
+    const lidParaRemover = jid.endsWith("@lid")
+      ? jid
+      : await sock.signalRepository.lidMapping.getLIDForPN(jid);
+    await sock.sendMessage(jid, {
+      text: `✅ *Atendimento encerrado.*\nObrigado pela preferência! Se precisar de algo, é só chamar novamente.`,
+    });
+
+    if (lidParaRemover) {
+      atendimentos.delete(lidParaRemover);
+      console.log(`Atendimento encerrado para ${lidParaRemover}`);
+    }
+    return;
+  }
+
+  if (fromMe || !msg) return;
+
+
+  if (atendimentos.get(jid) === "humano") return;
+
+  if (msg === "atendente") {
+    await sock.sendMessage(jid, {
+      text: "Um atendente humano irá te responder em breve. Obrigado!",
+    });
+    atendimentos.set(jid, "humano");
+    return;
+  }
+
+  const estadoAtual = atendimentos.get(jid) || "inicio";
+
+  if (estadoAtual === 'inicio') {
+        const menuInicial = "Você está falando diretamente com a *AKIJOGA LOTERIAS*, seja bem vindo!\n\nEscolha uma opção:\n1 - Participar de um bolão\n2 - Fazer um jogo";
+        await sock.sendMessage(jid, { text: menuInicial });
+        atendimentos.set(jid, 'menu_principal');
+        return;
+    }
+
+    if (estadoAtual === 'menu_principal') {
+        if (msg === '1') {
+            const menuLoterias = "ESCOLHA A MODALIDADE:\nDigite o numero referente a sua escolha\n\n1 - MEGA SENA\n2 - LOTOFÁCIL\n3 - QUINA\n4 - + MILIONÁRIA\n5 - TIMEMANIA\n6 - DUPLA SENA\n7 - SUPER SETE\n8 - DIA DE SORTE\n9 - LOTOMANIA";
+            await sock.sendMessage(jid, { text: menuLoterias });
+            atendimentos.set(jid, 'menu_loterias');
+        } else if (msg === '2') {
+            await sock.sendMessage(jid, { text: "OK!\nEm breve um atendente irá falar com você" });
+            atendimentos.set(jid, 'atendimento');
+        } else {
+            await sock.sendMessage(jid, { text: "⚠️ Não entendi.\n\nEscolha uma opção:\n1 - Participar de um bolão\n2 - Fazer um jogo" });
+        }
+        return;
+    }
+
+    if (estadoAtual === 'menu_loterias') {
+        const opcoesValidas = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+        if (opcoesValidas.includes(msg)) {
+            await sock.sendMessage(jid, { text: "OK!\nEm breve um atendente irá falar com você" });
+            atendimentos.set(jid, 'atendimento');
+        } else {
+            await sock.sendMessage(jid, { text: "⚠️ Opção inválida. Digite o número de 1 a 9 referente à loteria desejada." });
+        }
+        return;
+    }
+
+}
